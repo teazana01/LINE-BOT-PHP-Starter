b974fdd6aac8874cd3dd15c68977daf7feea985f
teazana01 <37362062+teazana01@users.noreply.github.com>
2018-03-14 15:00:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-echo "Hello LINE BOT";
-echo "Hello LINE BOT";
-echo "Hello LINE BOT";
-echo "Hello LINE BOT";
-echo "Hello LINE BOT";
-echo "Hello LINE BOT";
+echo "Hello LINE sdffsdsdfddfdfBOT";
+
